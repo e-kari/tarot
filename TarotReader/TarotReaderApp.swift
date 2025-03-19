b4d3a780c221622310d2913d906240c88eb32981
Kari <enya.kpg@gmail.com>
2025-03-18 22:23:30 -0500

    Initial Commit

---
+++ TarotReader/TarotReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotReaderApp.swift
+//  TarotReader
+//
+//  Created by Karina on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TarotReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
